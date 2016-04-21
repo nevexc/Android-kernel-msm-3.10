be1d0347e7be0d356c5d52220c34498ed7176b9a
jiazhi.lin <jiazhi.lin@tcl.com>
2016-04-21 16:40:50 +0800

    TCL bringup touchpad ft5626 & s7060b
    Change-Id: I7284499da08c74a6dec71c495301383facbd1235

---
+++ include/linux/lcd_monitor.h
@@ -0,0 +1,9 @@
+#ifndef __LCD_MONITOR_H__
+#define __LCD_MONITOR_H__
+#define KERNEL_FIRST_LCD "90f006f5"
+#define KERNEL_FIRST_TP "7ca37b93"
+#define KERNEL_SECOND_LCD "eab0e4db"
+#define KERNEL_SECOND_TP "5f8e3e98"
+#define LK_FIRST_LCD "3f925cde"
+#define LK_SECOND_LCD "1ca58d97"
+#endif
